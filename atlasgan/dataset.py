5026ff4676734c8d925c76eb31f96648fb9f9bb0
Steve Farrell <sparticlesteve@gmail.com>
2018-05-31 07:34:20 -0700

    added dataset description of basic RPV image data

---
+++ atlasgan/dataset.py
@@ -0,0 +1,27 @@
+"""
+This module defines the ATLAS RPV images datasets.
+"""
+
+# External imports
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+class RPVImages(Dataset):
+    """Dataset wrapping RPV image tensors."""
+    def __init__(self, input_file, n_samples=None, from_back=False):
+        # Load the data
+        with np.load(input_file) as f:
+            fdata = f['hist']
+            if n_samples is not None and n_samples > 0:
+                self.data = torch.from_numpy(fdata[:n_samples, None].astype(np.float32))
+                #self.data = fdata[:n_samples].copy()
+                #del fdata
+            else:
+                self.data = torch.from_numpy(fdata[:, None]).astype(np.float32)
+    
+    def __getitem__(self, index):
+        return self.data[index]
+    
+    def __len__(self):
+        return self.data.size(0)
